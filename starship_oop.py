c4bfe1d33bfcdbf978c7f1563afd9d990b9d258f
Strawberry-Swim <turaojd30@wvc.pshs.edu.ph>
2026-09-02 16:38:29 +0800

    Add files via upload

---
+++ starship_oop.py
@@ -0,0 +1,21 @@
+class Starship:
+
+    def __init__(self, base_weight, cargo_weight, final_fuel):
+        self.base_weight = base_weight
+        self.cargo_weight = cargo_weight
+        self.final_fuel = final_fuel
+
+    def Starship(self, base_weight, cargo_weight, final_fuel):
+            base_weight = 500000
+             cargo_weight == ""
+            final_fuel=(base_weight + cargo_weight) * 3
+
+    def calculate_fuel(self):
+        cargo_weight = 0
+
+Starship = Starship(500000)
+Starship.load_cargo()
+Starship.load_cargo()
+Starship.load_cargo()
+
+Starship.calculate_fuel()
